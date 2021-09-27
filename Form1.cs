ef9da1daea2f651e2ae99d148c39ad514043b6e0
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-09-27 14:57:35 -0500

    Forms Complejo

--- Form1.cs
+++ Form1.cs
@@ -23,6 +23,7 @@ namespace FormComplejos_G4_2022_I
             Complejo c1 = new Complejo(float.Parse(txtbRelaComplejo1.Text),float.Parse(txtbImaginariaComplejo1.Text));
             Complejo c2 = new Complejo(float.Parse(txtbRealComplejo2.Text), float.Parse(txtbImaginariaComplejo2.Text));
             Complejo c3 = c1 + c2;
+            
             lbSumaComplejo.Text = c3.ToString();
         }
     }
